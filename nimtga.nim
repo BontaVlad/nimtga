a85cadcec7b54594166b84b5ffc7b7f49fe2fd20
Bonta Vlad <vbonta@neverfail.com>
2016-10-14 19:27:00 +0300

    Binary stuff is ... something.

--- nimtga.nim
+++ nimtga.nim
@@ -95,6 +95,11 @@ type
     header: Header
     footer: Footer
     new_tga_format: bool
+    first_pixel: int
+    bottom_left: int
+    bottom_right: int
+    top_left: int
+    top_right: int
     pixels: seq[seq[Pixel]]
 
 proc get_rgb_from_16(data: int16): tuple[r, g, b: int] =
@@ -106,9 +111,9 @@ proc get_rgb_from_16(data: int16): tuple[r, g, b: int] =
     Returns:
         tuple(int, int, int): the RGB color
     ]#
-    let c_r = (data and 0b1111100000000000) shr 11
-    let c_g = (data and 0b0000011111000000) shr 6
-    let c_b = (data and 0b111110) shr 1
+    let c_r = cast[uint8]((data and 0b1111100000000000) shr 11)
+    let c_g = cast[uint8]((data and 0b0000011111000000) shr 6)
+    let c_b = cast[uint8]((data and 0b111110) shr 1)
     result.r = c_r.int
     result.g = c_g.int
     result.b = c_b.int
@@ -117,13 +122,50 @@ proc ImageNew(): Image =
   result.header = Header()
   result.footer = Footer()
   result.pixels = @[]
+  result.pixels.add(@[])
   result.new_tga_format = false
 
+  # Screen destination of first pixel
+  result.bottom_left = 0b0
+  result.bottom_right = 0b1 shl 4
+  result.top_left = 0b1 shl 5
+  result.top_right = 0b1 shl 4 or 0b1 shl 5
+
+  # Default values
+  result.first_pixel = result.top_left
+
+
 proc load*(self: var Image, file_name: string) =
+
+  template to_int(expr: untyped): int =
+    cast[uint8](expr).int
+
+  proc parse_pixel(self: var Image, fs: var FileStream): Pixel =
+    if self.header.image_type in [3, 11]:
+      let val = fs.readInt8().to_int
+      # echo val
+      result = Pixel(kind: pkBW, bw_val: val)
+    elif self.header.image_type in [2, 10]:
+      case self.header.pixel_depth
+      of 16:
+        result = Pixel(
+          kind: pkRGB,
+          rgb_val: get_rgb_from_16(fs.readInt16()))
+      of 24:
+        result = Pixel(
+          kind: pkRGB,
+          rgb_val: (fs.readInt8().to_int, fs.readInt8().to_int, fs.readInt8().to_int)
+        )
+      of 32:
+        result = Pixel(
+          kind: pkRGBA,
+          rgba_val: (fs.readInt8().to_int, fs.readInt8().to_int, fs.readInt8().to_int, fs.readInt8().to_int)
+        )
+      else: raise newException(ValueError, "unsupported image type")
+
   var
     f: File
     fs: FileStream
-    # fs = newFileStream(file_name, fmRead)
   if not open(f, file_name, fmRead):
     raise newException(IOError, "Failed to open file: $#" % file_name)
 
@@ -146,6 +188,8 @@ proc load*(self: var Image, file_name: string) =
   self.header.pixel_depth = fs.readInt8().int
   self.header.image_descriptor = fs.readInt8().int
 
+  let original_position = fs.getPosition()
+
   fs.setPosition(getFileSize(f).int - 26)
   self.footer.extension_area_offset = fs.readInt32().int
   self.footer.developer_directory_offset = fs.readInt32().int
@@ -155,43 +199,142 @@ proc load*(self: var Image, file_name: string) =
 
   if self.footer.signature == "TRUEVISION-XFILE":
     self.new_tga_format = true
-
-  echo repr(self)
+  fs.setPosition(original_position)
 
   # no compression
   if self.header.image_type in [2, 3]:
-    for row in 0 .. self.header.image_height:
+    for row in 0 .. self.header.image_height - 1:
       self.pixels.add(@[])
-      for col in 0 .. self.header.image_width:
-        var pixel: Pixel
-        if self.header.image_type == 3:
-          pixel = Pixel(kind: pkBW, bw_val: fs.readInt8().int)
-        elif self.header.image_type == 2:
-          discard
-          case self.header.pixel_depth
-          of 16:
-            pixel = Pixel(
-              kind: pkRGB,
-              rgb_val: get_rgb_from_16(fs.readInt16()))
-          of 24:
-            pixel = Pixel(
-              kind: pkRGB,
-              rgb_val: (fs.readInt8().int, fs.readInt8().int, fs.readInt8().int)
-            )
-          of 32:
-            pixel = Pixel(
-              kind: pkRGBA,
-              rgba_val: (fs.readInt8().int, fs.readInt8().int, fs.readInt8().int, fs.readInt8().int)
-            )
-          else: raise newException(ValueError, "unsupported image type")
-
-        self.pixels[row].add(pixel)
+      for col in 0 .. self.header.image_width - 1:
+        self.pixels[row].add(self.parse_pixel(fs))
   # compressed
   elif self.header.image_type in [10, 11]:
-    echo "compressed"
-    self.pixels.add(@[])
-    let tot_pixels = self.header.image_height * self.header.image_width
-    var pixel_count = 0
+    # echo "compressed"
+    let
+      tot_pixels = self.header.image_height * self.header.image_width
+    var
+      pixel_count = 0
+      row = 0
+    while pixel_count <= tot_pixels:
+      if self.pixels[row].high >= self.header.image_width:
+        self.pixels.add(@[])
+        # echo self.pixels[row].high
+        # echo row
+        inc(row)
+      let repetition_count = fs.readInt8()
+      let RLE: bool = (repetition_count and 0b10000000) shr 7 == 1
+      let count: int = (repetition_count and 0b01111111).int + 1
+      pixel_count += count
+      # echo pixel_count
+      if RLE:
+        let pixel = self.parse_pixel(fs)
+        for num in 0 .. count:
+          self.pixels[row].add(pixel)
+      else:
+        for num in 0 .. count:
+          self.pixels[row].add(self.parse_pixel(fs))
+
+template write_value(f: typed, data: typed) =
+  # echo data
+  let data_size = sizeof(data)
+  let written_data = f.writeBuffer(addr data, data_size)
+  assert written_data == data_size
+
+proc write_header(f: var File, image: ptr Image) =
+  f.write_value(image.header.id_length)
+  f.write_value(image.header.color_map_type)
+  f.write_value(image.header.image_type)
+  f.write_value(image.header.first_entry_index)
+  f.write_value(image.header.color_map_length)
+  f.write_value(image.header.color_map_entry_size)
+  f.write_value(image.header.x_origin)
+  f.write_value(image.header.y_origin)
+  f.write_value(image.header.image_width)
+  f.write_value(image.header.image_height)
+  f.write_value(image.header.pixel_depth)
+  f.write_value(image.header.image_descriptor)
+
+proc write_footer(f: var File, image: ptr Image) =
+  f.write_value(image.footer.extension_area_offset)
+  f.write_value(image.footer.developer_directory_offset)
+  f.write_value(image.footer.signature)
+  f.write_value(image.footer.dot)
+  f.write_value(image.footer.eend)
+
+proc save*(self: var Image, filename: string, compress=false, force_16_bit=false) =
+  echo self.pixels[0].len
+  echo self.pixels.len
+  # ID LENGTH
+  self.header.id_length = 0
+  # COLOR MAP TYPE
+  self.header.color_map_type = 0
+  # COLOR MAP SPECIFICATION
+  self.header.first_entry_index = 0
+  self.header.color_map_length = 0
+  self.header.color_map_entry_size = 0
+  # IMAGE SPECIFICATION
+  self.header.x_origin = 0
+  self.header.y_origin = 0
+  self.header.image_width = self.pixels[0].len
+  self.header.image_height = self.pixels.len
+  self.header.image_descriptor = 0b0 or self.first_pixel
+
+  # IMAGE TYPE
+  # IMAGE SPECIFICATION (pixel_depht)
+  let tmp_pixel = self.pixels[0][0]
+  case tmp_pixel.kind
+  of pkBW:
+    self.header.image_type = 3
+    self.header.pixel_depth = 8
+  of pkRGB:
+    self.header.image_type = 2
+    if force_16_bit:
+      self.header.pixel_depth = 16
+    else:
+      self.header.pixel_depth = 24
+  of pkRGBA:
+    self.header.image_type = 2
+    self.header.pixel_depth = 32
+  else: raise newException(ValueError, "invalid pixel kind")
+
+  if compress:
+    case self.header.image_type
+    of 3: self.header.image_type = 11
+    of 2: self.header.image_type = 10
+    else: discard
+
+  var f = open(filename & ".tga", fmWrite)
+  defer: f.close()
+  if isNil(f):
+    raise newException(IOError, "Failed to open/create file: $#" % filename)
+
+  var p: Pixel
+  f.write_header(addr self)
+  if not compress:
+    for row in self.pixels:
+      for pixel in row:
+        case self.header.image_type
+        # of 3: p = pixel; f.write_value(p)
+        of 3: discard
+        of 2:
+          case self.header.pixel_depth
+          of 16: discard
+          of 24: discard
+          of 32: discard
+          else: raise newException(ValueError, "invalid pixel depth")
+        else: raise newException(ValueError, "invalid pixel kind")
+        # if self._header.image_type == 3:
+        # elif self._header.image_type == 2:
+        #   if self._header.pixel_depht == 16:
+        #     image_file.write(gen_pixel_rgb_16(*pixel))
+        #   elif self._header.pixel_depht == 24:
+        #     image_file.write(gen_pixel_rgba(*pixel))
+        #   elif self._header.pixel_depht == 32:
+        #     image_file.write(gen_pixel_rgba(*pixel))
+
+  f.write_footer(addr self)
 
 var image = ImageNew()
-image.load("african_head_diffuse.tga")
+image.load("image_bw.tga")
+image.save("ceva")
+echo repr(image.header)
